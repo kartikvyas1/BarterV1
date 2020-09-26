d02c7bc1569b535f7893c089151cde27a7d0a012
kartik vyas <kvyas@qti.qualcomm.com>
2020-09-27 00:36:16 +0530

    added firebase support

--- BApp/app/src/main/java/com/example/barterv1/MainActivity.java
+++ BApp/app/src/main/java/com/example/barterv1/MainActivity.java
@@ -6,10 +6,16 @@ import android.text.NoCopySpan;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
     }
 }
